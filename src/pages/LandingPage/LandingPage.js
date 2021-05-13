edb9a1a8fd31f81f2e6ac705b731b5fb077e2de8
Adam Doherty <ADoherty@inrule.com>
2021-05-13 17:40:59 -0500

    Update

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useHistory } from "react-router-dom";
 
-
 const LandingPage = () => {
   let history = useHistory();
   useEffect(() => {
